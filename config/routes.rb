14b44ed0dd56fbf6ca0b207177d9857b1994eeb5
dillonporter <dillonporter@gmail.com>
2018-03-07 09:29:13 -0700

    Styled audit log dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :audit_logs
-  resources :auditlogs
+  resources :audit_logs, except: [:new, :edit, :destroy]
   namespace :admin do
     resources :users
     resources :posts
