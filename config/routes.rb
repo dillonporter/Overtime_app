7bc599b198a1102ec386dbe19df3ff4fabf31bc5
dillonporter <dillonporter@gmail.com>
2018-03-07 12:08:11 -0700

    Created custom action buttons for Admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   resources :audit_logs, except: [:new, :edit, :destroy]
   namespace :admin do
-    resources :users
     resources :posts
+    resources :users
     resources :admin_users
 
     root to: "users#index"
   end
 
-  resources :posts
   devise_for :users, skip: [:registrations]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts do
+      member do
+        get :approve
+      end
+    end
 
   root to: 'static#homepage'
 end
